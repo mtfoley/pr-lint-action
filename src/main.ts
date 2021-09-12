3e1cb5685e22195f62d22b9b5b956779faf2aeed
mtfoley <matthewfoley@Matthews-MacBook-Pro.local>
2021-09-12 11:34:09 -0400

    conventional commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 //import { OctokitResponse, PullsListReviewsResponseData } from "@octokit/types";
 import lint from '@commitlint/lint';
+import load from '@commitlint/load';
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
@@ -17,25 +18,29 @@ const messageFilesNotMatched: string = core.getInput("message-files-not-matched"
 async function run(): Promise<void> {
   const githubContext = github.context;
   const pullRequest = githubContext.issue;
-
   const bodyRegex = new RegExp(bodyRegexInput,"im");
   const body: string =
     (githubContext.payload.pull_request?.body as string) ?? "";
   const title: string =
     (githubContext.payload.pull_request?.title as string) ?? "";
-  const titleValid = await lint(title).then(report => report.valid);
+  const {valid:titleValid,errors:lintErrors} = await checkTitle(title); 
   const files = await listFiles({...pullRequest,pull_number:pullRequest.number});
   const filesTripped = files.filter(f => filesToWatch.includes(f.filename)).length > 0;
-  makeComment(githubContext,pullRequest.number,titleValid,bodyRegex.test(body),filesTripped);
+  makeComment(githubContext,pullRequest.number,titleValid,lintErrors,bodyRegex.test(body),filesTripped);
+}
+async function checkTitle(title:string){
+  const lintOpts = await load({extends: ['@commitlint/config-conventional']});
+  return await lint(title,lintOpts.rules,lintOpts);
 }
 async function listFiles(pullRequest: {owner: string, repo: string, pull_number: number}) {
   const {data: files} = await githubClient.pulls.listFiles(pullRequest);
   return files;
 }
-async function makeComment(context: any, number:number, titleValid: boolean, linkedIssue: boolean, filesMatched: boolean) {
+async function makeComment(context: any, number:number, titleValid: boolean, lintErrors: any[], linkedIssue: boolean, filesMatched: boolean) {
   let message = "### Pull Request Linter\n";
+  const lintErrorsMessage: string = lintErrors.map(e=>e.message).join("\n");
   message += (titleValid ? messageTitleValid : messageTitleInvalid) + "\n";
-  message += (linkedIssue ? messageIssueValid : messageIssueInvalid) + "\n";
+  message += (linkedIssue ? messageIssueValid : (messageIssueInvalid + "Lint Errors:" + lintErrorsMessage)) + "\n";
   message += (filesMatched ? messageFilesMatched : messageFilesNotMatched) + "\n";
   // short circuit if nothing to say.
   if(message.trim() == "") return;
