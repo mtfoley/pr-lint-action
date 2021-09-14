89464fc700cb8ffbe57cda269e44616b2e2a5aa7
Matthew <mtfoley.mae@gmail.com>
2021-09-14 10:43:42 -0400

    check branch that PR is coming from

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,15 @@ const repoTokenInput = core.getInput("repo-token", { required: true });
 const githubClient = github.getOctokit(repoTokenInput);
 const bodyRegexInput: string = core.getInput("body-regex");
 const filesToWatch: string[] = core.getInput("filenames").split(/\s/).filter(f => f.trim() !== "");
+const protectBranch: string = core.getInput("protect-branch");
 const messageTitleValid: string = core.getInput("message-title-valid");
 const messageTitleInvalid: string = core.getInput("message-title-invalid");
 const messageIssueValid: string = core.getInput("message-issue-valid");
 const messageIssueInvalid: string = core.getInput("message-issue-invalid");
 const messageFilesMatched: string = core.getInput("message-files-matched");
 const messageFilesNotMatched: string = core.getInput("message-files-not-matched");
+const messageValidBranch: string = core.getInput("message-valid-branch");
+const messageInvalidBranch: string = core.getInput("message-invalid-branch");
 
 async function run(): Promise<void> {
   const githubContext = github.context;
@@ -25,7 +28,9 @@ async function run(): Promise<void> {
   const {valid:titleValid,errors:lintErrors} = await checkTitle(title); 
   const files = await listFiles({...pullRequest,pull_number:pullRequest.number});
   const filesTripped = files.filter(f => filesToWatch.includes(f.filename)).length > 0;
-  makeComment(githubContext,pullRequest.number,titleValid,lintErrors,bodyRegex.test(body),filesTripped);
+  const headRef: string = (githubContext.payload.pull_request?.head?.ref as string) ?? "";
+  let invalidBranch = (protectBranch !== "" && headRef == protectBranch);
+  makeComment(githubContext,pullRequest.number,titleValid,lintErrors,bodyRegex.test(body),filesTripped,invalidBranch,headRef);
 }
 async function checkTitle(title:string){
   return await lint(title,conventionalOpts.rules,conventionalOpts);
@@ -34,13 +39,14 @@ async function listFiles(pullRequest: {owner: string, repo: string, pull_number:
   const {data: files} = await githubClient.pulls.listFiles(pullRequest);
   return files;
 }
-async function makeComment(context: any, number:number, titleValid: boolean, lintErrors: any[], linkedIssue: boolean, filesMatched: boolean) {
+async function makeComment(context: any, number:number, titleValid: boolean, lintErrors: any[], linkedIssue: boolean, filesMatched: boolean, invalidBranch: boolean, branch: string) {
   let message = "";
   const lintErrorsMessage: string = lintErrors.map(e=>e.message).join("\n");
   message += (titleValid ? messageTitleValid : messageTitleInvalid) + "\n";
   message += (linkedIssue ? messageIssueValid : (messageIssueInvalid + "\nLint Errors:" + lintErrorsMessage)) + "\n";
   message += (filesMatched ? messageFilesMatched : messageFilesNotMatched) + "\n";
-  // short circuit if nothing to say.
+  message += (invalidBranch ? messageInvalidBranch.replace("%branch%",branch) : messageValidBranch) + "\n";
+  // short circuit if nothing to say
   if(message.trim() == "") return;
   await githubClient.issues.createComment({
     ...context.repo,
